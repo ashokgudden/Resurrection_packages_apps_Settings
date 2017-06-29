133fba856046c1f8b6fd68782764e57022e40828
Sam Mortimer <sam@mortimer.me.uk>
2017-06-29 10:04:42 +0530

    [3/3] Torch long press power: add auto-off function
    Change-Id: I0463df7747356b62c1c510e7f3f79f6692a2b77c

--- src/com/android/settings/rr/LockScreenGesture.java
+++ src/com/android/settings/rr/LockScreenGesture.java
@@ -14,6 +14,8 @@
 package com.android.settings.rr;
 
 import android.os.Bundle;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
@@ -26,10 +28,14 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import cyanogenmod.providers.CMSettings;
+
 public class LockScreenGesture extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockScreenGesture";
-
+    private static final String KEY_TORCH_LONG_PRESS_POWER_TIMEOUT = "torch_long_press_power_timeout";
+    private ListPreference mTorchLongPressPowerTimeout;
+    private ListPreference list;
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.RESURRECTED;
@@ -38,11 +44,39 @@ public class LockScreenGesture extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.rr_ls_gesture);
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final int torchLongPressPowerTimeout = CMSettings.System.getInt(resolver,
+                CMSettings.System.TORCH_LONG_PRESS_POWER_TIMEOUT, 0);
+        mTorchLongPressPowerTimeout = initList(KEY_TORCH_LONG_PRESS_POWER_TIMEOUT,
+                torchLongPressPowerTimeout);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) 		{
-        return true;
+        if (preference == mTorchLongPressPowerTimeout) {
+            handleListChange(mTorchLongPressPowerTimeout, objValue,
+                    CMSettings.System.TORCH_LONG_PRESS_POWER_TIMEOUT);
+            return true;
+        }
+        return false;
+    }
+
+    private ListPreference initList(String key, int value) {
+        try {             
+            list = (ListPreference) getPreferenceScreen().findPreference(key);
+        } catch (NullPointerException ex) {
+        }
+        if (list == null) return null;
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
+
+    private void handleListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
+        pref.setSummary(pref.getEntries()[index]);
+        CMSettings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
     }
 }
